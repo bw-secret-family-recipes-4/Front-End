50f676225923e0e2d2ba247de34764555fcd4e38
victor <victordronov1@gmail.com>
2020-09-21 10:31:29 -0400

    testing routeing

--- family-recipes/src/App.js
+++ family-recipes/src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
-    Chicken
+      Chicken
       <Switch>
-        <Route>
-
+        <Route to="/Login">
+          <Login />
         </Route>
-        <Route>
-
-        </Route>
+        <Route></Route>
       </Switch>
     </>
   );
--- family-recipes/src/components/Login.js
+++ family-recipes/src/components/Login.js
@@ -1,1 +1,12 @@
 import React from "react"
+
+
+const Login = ()=>{
+    return (
+        <>
+        Login Form Here
+        </>
+    )
+}
+
+export default Login
