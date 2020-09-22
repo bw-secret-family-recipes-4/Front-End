ddff792d24ee437486b995c31bed2c01c2ffaab4
victor dronov <victordronov1@gmail.com>
2020-09-22 15:18:40 -0400

    form for adding a recipe build out needs to be linked to post

--- family-recipes/src/dashboardComponents/FamilyRecipesForm.js
+++ family-recipes/src/dashboardComponents/FamilyRecipesForm.js
@@ -1,23 +1,89 @@
 import React, { useState } from "react";
 
-const initialForm = {
-  ingredients: "",
+const initialAuthor = {
+  title: "",
+  source: "",
+  category: "",
 };
-
+const initialIng = {
+  ingredient_name: [], //WIll have to loop through
+};
+const initialInstructions = {
+  instructions: "",
+};
 const AddRecipes = () => {
-  const [form, setform] = useState(initialForm);
+  const [form, setform] = useState(initialAuthor);
+  const [Ingredient, setIngredient] = useState(initialIng);
+  const [instructions, setInstrictions] = useState(initialInstructions);
 
   const submit = () => {
-    setform(initialForm);
+    setform(initialAuthor);
+    setIngredient(initialIng);
   };
+
+  const formChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setform({ ...form, [name]: value });
+  };
+  const ingredientsChange = (e) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setIngredient({ ...Ingredient, ingredient_name: value.split(",") });
+  };
+
+  const instructionChange = (e) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setInstrictions({ ...instructions, instructions: value });
+  };
+
   // REMEMBER TO GET USER ID FROM THE LOCAL STORAGE WHEN POSTING OTHER WISE IT WILL FAIL!!!!
   return (
     <>
       <form onSubmit={submit}>
         <label>
-          Ingredient:
-          <input type="text" value={form.ingredients} />
+          Recipe Name:
+          <input
+            type="text"
+            name="title"
+            value={form.title}
+            onChange={formChange}
+          />
         </label>
+        <label>
+          Author:
+          <input
+            type="text"
+            name="source"
+            value={form.source}
+            onChange={formChange}
+          />
+        </label>
+        <label>
+          Catagory:
+          <select name="category" onChange={formChange}>
+            <option> Choose A Catagory!</option>
+            <option value="Fast Food">Fast Food</option>
+            <option value="Home">Home</option>
+            <option value="I Have No Idea">I Have No Idea</option>
+          </select>
+        </label>
+        <textarea
+          cols="100"
+          row="100"
+          value={Ingredient.ingredient_name}
+          placeholder="Type your ingredients here seperated by a coma. EX: sugar, beans, ..."
+          onChange={ingredientsChange}
+        />
+        <textarea
+          cols="100"
+          row="100"
+          // name="instructions"
+          value={instructions.instructions}
+          placeholder="Type your instructions here"
+          onChange={instructionChange}
+        />
         <button>Add Recipe</button>
       </form>
     </>
